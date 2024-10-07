216521989533a07e382450c79bef6fae4b48c1a8
Maksim <i@mgromov.ru>
2024-10-07 18:30:06 +0300

    1

--- HW29/ViewController.swift
+++ HW29/ViewController.swift
@@ -6,13 +6,55 @@
 //
 
 import UIKit
+// MARK: - User
+struct User: Codable {
+    let id: Int
+    let name, username, email: String
+    let address: Address
+    let phone, website: String
+    let company: Company
+}
+
+// MARK: - Address
+struct Address: Codable {
+    let street, suite, city, zipcode: String
+    let geo: Geo
+}
+
+// MARK: - Geo
+struct Geo: Codable {
+    let lat, lng: String
+}
+
+// MARK: - Company
+struct Company: Codable {
+    let name, catchPhrase, bs: String
+}
 
 class ViewController: UIViewController {
-
+    var users: [User] = []
+    func fetchData() {
+        print("fetching data...")
+        let url = URL(string: "https://jsonplaceholder.typicode.com/users")!
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data else { return }
+            do {
+                self.users = try JSONDecoder().decode([User].self, from: data)
+                for u in self.users {
+                    print("User: \(u.name), email: \(u.email), phone: \(u.phone), company: \(u.company.name)")
+                }
+            } catch {
+                print(error)
+            }
+        }.resume()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchData()
         // Do any additional setup after loading the view.
     }
+    
 
 
 }
